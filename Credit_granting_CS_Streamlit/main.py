eaab34321c38a477e94c3d51929d675eaf9fa6cc
vp_vinc <vp.vincentelli@gmail.com>
2021-09-06 18:44:44 +0200

    dashboard V1 finished / num_cat_cols pkl added for avg graphs / st_state_patch added to use multiple buttons in dashboard

--- Credit_granting_CS_Streamlit/main.py
+++ Credit_granting_CS_Streamlit/main.py
@@ -5,106 +5,100 @@ import pandas as pd
 import sklearn
 import matplotlib
 import matplotlib.pyplot as plt
+
 import shap
 import dill
 import streamlit as st
 import streamlit.components.v1 as components
+from helper import*
+import st_state_patch
+seed=seed
 
+# set page config
+st.set_page_config(page_title="Loan_scoring_dashboard", # or None
+                          page_icon="U+1F3E6", # or None
+                          layout='wide', # or 'centered' for wide margins
+                          initial_sidebar_state='auto')
 
-# importing and unpickling train df, explainer, shap values and model
-@st.cache
-def load_train_data():
-    """returns train dataset"""
-    return pd.read_csv(
-        "C:/Users/VP/Google Drive/Education/OC/working_directory/P7/train.csv").iloc[:, 1:]  # skip Unamed column
-
-
-@st.cache
-def load_prep_train_data():
-    """returns train dataset preprocessed. Indeed the pipeline select 50 features, and we need the names of these 50
-    columns to display feature' names in waterfall shap graph"""
-    return pd.read_csv(
-        "C:/Users/VP/Google Drive/Education/OC/working_directory/P7/prep_train.csv").iloc[:, 1:]  # skip Unamed
-
-
-def load_explainer_shapvs():
-    """returns tuple (fitted explainer, shap values)"""
-    with open(
-            "C:/Users/VP/Google Drive/Education/OC/working_directory/P7/Credit_granting_CS_Streamlit/explainer_shapvs"
-            ".pkl",
-            "rb") as file:
-        return dill.load(file)
-
-
-def load_model():
-    """returns fitted model"""
-    with open("C:/Users/VP/Google Drive/Education/OC/working_directory/P7/Credit_granting_CS_Streamlit/model.pkl",
-              "rb") as file:
-        best_model = dill.load(file)
-    return best_model
-
-
-# defining the function for the custom threshol function
-def cust_predict_proba(pos_class_proba_array, thres=0.5):
-    """This is a threshold function that convert an array of probabilities of belonging to class "1" into a array of
-    labels 0 or 1 with respect to the threshold. We will use this function to set up an optimal threshold that minimize
-    false negative, unlike the default value of 0.5"""
-    pos_class_proba_array = pd.Series(pos_class_proba_array)
-    pos_class_proba_array = pos_class_proba_array.map(lambda x: 1 if x > thres else 0)
-    return pos_class_proba_array.to_numpy()
-
-
-# defining function to display default instead of 1
-def display_pred(prediction):
-    if prediction == 0:
-        return 'No default'
-    else:
-        return 'Default'
-
-
-# defining function to display shap force plot in streamlit
-def st_shap(plot, height=None):
-    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
-    components.html(shap_html, height=height)
-
+# variable to display average graphs only once shap graphs are displayed
+display_avg = False
 
 # title
 st.title("Credit default probability per client")
 st.subheader("The prediction is then analysed by breaking down most influential variables for the prediction")
 
-# Collecting input fed by user
-with st.form(key='my_form'):
-    client_id = st.text_input(label='Enter client SK_CURR_ID to assess')
-    # client_id = int(client_id)
+# load data
+with st.spinner('Loading data and prediction model...'):
+    train = load_raw_application_train_data()
+    prep_train = load_prep_train_data()
+    num_cols, cat_cols = load_num_cat_cols()
+    explainer, shapvs = load_explainer_shapvs()
+    pipe = load_pipe()
+    model = pipe.named_steps['LBMC']
+
+# Collecting client id to assess in sidebar
+
+with st.sidebar.form(key='my_form'):
+    client_id = st.text_input(label='Enter client SK_ID_CURR to assess')
     submit_button = st.form_submit_button(label='Submit')
 
+# command to keep state of the buttons saved. Necessary to prevent streamlit from refreshing between two buttons hit
+s = st.State()
+if not s:
+    s.pressed_first_button = False
+
 # condition prediction to submit button
-if submit_button:
-    # load data
-    train = load_train_data()
-    prep_train = load_prep_train_data()
-    explainer, shapvs = load_explainer_shapvs()
-    model = load_model()
+if submit_button or s.pressed_first_button:
+    # asserting if the input ID is correct
+    if int(client_id) not in prep_train['SK_ID_CURR'].values:
+        raise AssertionError("This ID is either mispelled or not in the database. EX: 100004")
+    # preserve the info that you hit a button between runs
+    s.pressed_first_button = True
     # get index of client SK_CURR_ID
-    index_client_id = train['SK_ID_CURR'].index[train['SK_ID_CURR'] == int(client_id)][0]
+    index_client_id = prep_train['SK_ID_CURR'].index[prep_train['SK_ID_CURR'] == int(client_id)][0]
     # make the default probability
-    proba_def = model.predict_proba(train.iloc[index_client_id, 1:-1].values.reshape(1, -1))[0][1] # skip SK_CURR_ID column
+    proba_def = model.predict_proba(prep_train.iloc[index_client_id, :-1] # skip SK_CURR_ID last column
+                                    .values.reshape(1, -1))[0][1]
     # make the default prediction
     cust_prediction = cust_predict_proba(proba_def, thres=0.5)
     st.write("Prediction on credit refund: {}".format(display_pred(cust_prediction)))
     st.write("Probability of default: {:.1%}".format(proba_def))
     # waterfall graphs display
-    # st_shap(shap.force_plot(explainer.expected_value, shapvs[0, :], train.iloc[index_client_id, 1:-1]))
-    # st_shap(shap.plots._waterfall.waterfall_legacy(explainer.expected_value[1],
-    #                                               shapvs[1][index_client_id],
-    #                                               features=prep_train.iloc[index_client_id].values,
-    #                                              feature_names=prep_train.columns))
-
-    # visualize the prediction's explanation (use matplotlib=True to avoid Javascript)
-    st_shap(shap.force_plot(explainer.expected_value[1], shapvs[1][index_client_id], prep_train.iloc[index_client_id]))
-
-    # Summary plot 1 SHAP
-    st.subheader('Summary Plot')
+    st.subheader('Waterfall and force plot')
     fig, ax = plt.subplots(nrows=1, ncols=1)
-    shap.summary_plot(shapvs[1],  prep_train.values, prep_train.columns, max_display=30)
+    shap.plots._waterfall.waterfall_legacy(explainer.expected_value[1],
+                                           shapvs[1][index_client_id],
+                                           features=prep_train.iloc[index_client_id, :-1].values,
+                                           feature_names=prep_train.columns[:-1])
     st.pyplot(fig)
+    # visualize the prediction's explanation with force plot (use matplotlib=True to avoid Javascript)
+    st_shap(shap.force_plot(explainer.expected_value[1],
+                            shapvs[1][index_client_id],
+                            prep_train.iloc[index_client_id, :-1]) # excluding last column TARGET
+            )
+    st.write("The values of features in blue lower the probability of default whereas those in red increase it. \n"
+             "For example, DAYS_EMPLOYED is negative and measure the number of days the client was employed at the date"
+             " of the request. Therefore, a high value for DAYS_EMPlOYED indicates that the client has not been employed"
+             " for long and will be red because it increases the probability of default")
+    # Summary plot 1 SHAP
+    st.subheader('Summary Plot of all clients')
+    with st.spinner('plotting summary of influential variables for all clients...'):
+        dico_of_fig = summary_plot_all(shapvs, prep_train, train['Default(1)/No default(0)'])
+        st.pyplot(dico_of_fig['fig'])
+        st.write("/!\ DAYS_EMPLOYED is negative, so a low value implies a long period of employement")
+
+    display_avg = True
+
+if display_avg==True: #if shap graph have been displayed
+    st.subheader("Value of the client's feature VS the averages of default and non-default clients")
+    feature = st.selectbox('Select a feature', sorted(list(train.iloc[:,:-1].columns)))
+    st.write("the value of the feature for the client is: {}".format(train.loc[index_client_id, feature]))
+    if feature in num_cols:
+        fig, ax = plt.subplots(figsize=(25,15))
+        kde_plot_num_var(data=train, x='Default(1)/No default(0)', y=feature)
+        st.pyplot(fig)
+
+    if feature in cat_cols:
+        fig, ax = plt.subplots(figsize=(25,15))
+        bar_plot_cat_var(data=train, x=feature, hue='Default(1)/No default(0)')
+        st.pyplot(fig)
--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# P7_loan_scoring_model
-model for loan requests scoring + dashboard for customer service
+# Dashboard for interpreting credit granting predictions with SHAP
+
+---
+---
+This project aims at deploying a dashboard to consult predictions for the granting of the credit (default or not) of about 300k clients.
+The dataset comes from the Kaggle competition.
+## Table of content
+
+---
+1. Structure of the projet
+2. Selection and training of the model
+3. customized cost function and optimization
+4. Interpretability of the model using SHAP
+
+## 1. Structure of the projet
+
+---
+**This project articulates around 12 files:**
+- P7_EDA: notebook for EDA (adapted from [Will Koehrsen](https://www.kaggle.com/willkoehrsen/start-here-a-gentle-introduction) 's work)
+- P7_modelling: notebook containing:
+  - preprocessing steps 
+  - GridSearchCV for two models, 
+  - optimization of the best model with a custom metrics, 
+  - computing of a Tree explainer and SHAP values
+  - exportation of data, explainer and model to be used with Streamlit
+- main.py: application file to be run with streamlit. The user must type in an ID. The prediction (default/not default) 
+and the default probability are then displayed along with SHAP plots (waterfall, force-plot, summary plot)
+- helper.py: package containing functions used in main.py
+- prep_train.csv: dataset preprocessed. This file is not on the repo as per Kaggle policy
+- train.csv: dataset unprocessed (only imputation by median and mode). This file is not on the repo as per Kaggle policy
+- folder "data":
+  - explainer_shapvs.pkl: tree_explainer fitted with best model and shap values computed for the whole dataset. Both are 
+pickled together and use in main.py. The file is not available on Github due to size constraints
+  - pipe.pkl: best pipe pickled used in main.py
+  - num_cat_cols: pkl file containing lists used to display the last graph in the dashboard
+- environment.yml: file to set up dependencies with conda
+- requirements.txt: file to set up dependencies with pip
+
+## 2. Selection and training of the model
+
+---
+Two high performing classifiers were considered here: Random Forest Classifier (RFC) and Light GBM (LGBM).
+A cross-validated GridSearch was applied for these two model with the following grid for preprocessing steps:  
+- imputer: 
+  - SimpleImputer(strategy='median')
+  - SimpleImputer(strategy='mean')
+  - SimpleImputer(strategy='constant')
+- resampling: 
+  - SMOTE(sampling_strategy=0.8)
+  - RandomUnderSampler(sampling_strategy='majority')  
+- feature_selection: 
+  - RFE(estimator=DecisionTreeClassifier()
+  - n_features_to_select=50), SelectKBest(k=50)
+
+The grid of parameters for each step is available in the notebook P7_modelling
+
+## 3. customized cost function and optimization
+
+---
+
+
+## 4. Interpretability of the model using SHAP
+
+---
+
